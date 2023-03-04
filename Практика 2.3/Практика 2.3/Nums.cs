356421124191fe23ea03066b45e82f4cd0a89bfd
generalissimo12 <118387787+generalissimo12@users.noreply.github.com>
2023-03-04 14:28:02 +0700

    Add files via upload

---
+++ Практика 2.3/Практика 2.3/Nums.cs
@@ -0,0 +1,76 @@
+﻿using System;
+namespace Практика_2._3
+{
+    public class Nums
+    {
+        public int Num1 { get; set; }
+
+        public int Num2 { get; set; }
+
+        public Nums(int Num1, int Num2)
+        {
+            this.Num1 = Num1;
+            this.Num2 = Num2;
+
+        }
+
+
+
+        public void Sum()
+        {
+            Console.WriteLine($"Сумма чисел: {Num1 + Num2}");
+
+        }
+        public void ShowNum()
+        {
+            Console.WriteLine("Первое число равно: " + Num1);
+            Console.WriteLine("Второе число равно: " + Num2);
+
+        }
+        public void ChangeNum()
+        {
+            int input = 0;
+            while (input != 3)
+            {
+                Console.WriteLine("1 - Изменить первое число\n2 - Изменить второе число\n3 - Выйти в основное меню");
+                input = Convert.ToInt32(Console.ReadLine());
+                if (input == 3) break;
+                switch (input)
+                {
+                    case 1:
+                        Console.WriteLine("Введите желаемое число ");
+                        input = Convert.ToInt32(Console.ReadLine());
+                        Num1 = input;
+                        Console.WriteLine("Первое число равно: " + Num1);
+
+                        break;
+                    case 2:
+                        Console.WriteLine("Введите желаемое число ");
+                        input = Convert.ToInt32(Console.ReadLine());
+                        Num2 = input;
+                        Console.WriteLine("Второе число равно: " + Num2);
+                        break;
+                    case 3:
+                        break;
+                }
+            }
+        }
+        public void WhoIsBigger()
+        {
+            if (Num1 > Num2)
+            {
+                Console.WriteLine("Наибольшее число:" + Num1);
+            }
+            else
+            {
+                Console.WriteLine("Наибольшее число:" + Num2);
+            }
+        }
+
+        public void Menu()
+        {
+
+        }
+    }
+}
+
---
+++ Практика 2.3/Практика 2.3/Program.cs
@@ -0,0 +1,37 @@
+﻿using Практика_2._3;
+namespace Практика_2._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Nums res = new Nums(1, 5);
+
+
+            int input = 0;
+            while (input != 5)
+            {
+                Console.WriteLine("Меню:\n1 - Изменить значения чисел\n2 - Вывести сумму чисел\n3 - Вывести наибольшее из чисел\n4 - Вывести числа\n5 - Выход из программы");
+                if (input == 5) break;
+                input = Convert.ToInt32(Console.ReadLine());
+                switch (input)
+                {
+                    case 1:
+                        input = 0;
+                        res.ChangeNum();
+                        break;
+                    case 2:
+                        res.Sum();
+                        break;
+                    case 3:
+                        res.WhoIsBigger();
+                        break;
+                    case 4:
+                        res.ShowNum();
+                        break;
+                }
+
+            }
+        }
+    }
+}
