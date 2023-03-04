8408805fd897a9972eda2aac46030f472879b6f1
generalissimo12 <118387787+generalissimo12@users.noreply.github.com>
2023-03-04 14:27:12 +0700

    Add files via upload

---
+++ Практ 2 5/Практ 2 5/Del.cs
@@ -0,0 +1,37 @@
+﻿
+namespace Практ_2_5
+{
+    public class Del
+    {
+        private int a;
+        private int b;
+
+        public int A
+        {
+            get => this.a;
+            set => this.a = value;
+        }
+
+        public int B
+        {
+            get => this.b;
+            set => this.b = value;
+        }
+        public Del(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public Del()
+        {
+            a = 777;
+            b = 1111;
+        }
+
+        ~Del()
+        {
+            Console.Write($"{a} has been deleted\n");
+            Console.Write($"{b} has been deleted");
+        }
+    }
+}
---
+++ Практ 2 5/Практ 2 5/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+namespace Практ_2_5
+{
+    public class Program
+    {
+        public static void Method()
+        {
+            Del del = new Del();
+        }
+        static void Main()
+        {
+            Method();
+
+            GC.Collect();
+            Console.ReadLine();
+
+
+        }
+    }
+}
